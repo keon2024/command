9b8c4f5fbf92e89f03a39dba4b435c6d4f230cbe
songkeyang <songkeyang@jryghq.com>
2025-06-12 10:35:39 +0800

    增加 distinct 根据前多少个字符的去重能力

--- commands/distinct.go
+++ commands/distinct.go
@@ -3,15 +3,25 @@ package commands
 import (
 	"command/utils"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 type Distinct struct {
 	File string
+	Num  int
 }
 
 func (d *Distinct) Init(args []string) {
 	d.File = args[1]
+	if len(args) > 2 {
+		num, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println("num is not a number")
+			return
+		}
+		d.Num = num
+	}
 }
 
 func (d *Distinct) Check(args []string) bool {
@@ -30,19 +40,20 @@ func (d *Distinct) Exec() (bool, []string) {
 		flag   bool
 	)
 
-	flag, result = readFileDistinct(d.File)
+	flag, result = readFileDistinct(d.File, d.Num)
 
 	return flag, result
 }
 
 func (d *Distinct) Desc() {
-	var desc = `distinct <file> 
-	file: 文件路径`
+	var desc = `distinct <file> <num>
+	file: 文件路径
+	num: 读取每行的字符数，按读取每行前 num 个字符内容去重`
 	fmt.Println(desc)
 }
 
 // readFileDistinct 读取文件并去重
-func readFileDistinct(filePath string) (
+func readFileDistinct(filePath string, num int) (
 	bool, []string) {
 	var (
 		flag   bool
@@ -51,6 +62,10 @@ func readFileDistinct(filePath string) (
 	)
 	flag = utils.ReadFile(filePath, func(line string) bool {
 		value := strings.TrimSpace(line)
+		runes := []rune(value)
+		if num > 0 && len(runes) > num {
+			value = string(runes[:num]) // 截取前 num 个字符
+		}
 		if _, ok := dm[value]; !ok && value != "" {
 			dm[value] = 1
 			result = append(result, value)
